697e56846765304fce1b7384f6f8a3bc42439b2b
Zhou Jian <zhoujian0998@gmail.com>
2020-07-27 13:28:09 +0200

    db user table init sql

---
+++ sql/1_user.sql
@@ -0,0 +1,11 @@
+use `sf_academy_exchange_db`;
+
+drop table if exists `user`;
+
+create table `user` (
+    `id` int not null AUTO_INCREMENT primary key,
+    `email` varchar(256) not null check(`email` <> ""),
+    `password` varchar(64) not null check(`password` <> ""),
+    `username` varchar(64) not null check(`username` <> ""),
+    `date` datetime not null DEFAULT CURRENT_TIMESTAMP
+)
