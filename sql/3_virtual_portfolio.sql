6b7027b3c50e00ce9edbd5eddb0eaea4d0f43ea8
Zhou Jian <zhoujian0998@gmail.com>
2020-07-27 17:12:01 +0200

    deleted table user to virtual portfolio

--- sql/3_virtual_portfolio.sql
+++ sql/3_virtual_portfolio.sql
@@ -4,7 +4,9 @@ drop table if exists `virtual_portfolio`;
 
 create table `virtual_portfolio` (
     `id` int not null AUTO_INCREMENT primary key,
+    `user_id` int references `user`.`id` on update cascade on delete cascade,
     `currency` char(3) not null check(`currency` <> ""),
     `date` datetime not null DEFAULT CURRENT_TIMESTAMP,
-    `balance` decimal(15, 2) not null check(`balance` >= 0) default 0
+    `balance` decimal(15, 2) not null check(`balance` >= 0) default 0,
+    constraint user_unique_portfolio unique (`user_id`, `currency`)
 )
