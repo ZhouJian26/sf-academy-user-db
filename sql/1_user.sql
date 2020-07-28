18228ddee04dbd11dfdb7e4e66fc4b1f96c799b7
Zhou Jian <zhoujian0998@gmail.com>
2020-07-28 16:02:12 +0200

    unique email

--- sql/1_user.sql
+++ sql/1_user.sql
@@ -4,7 +4,7 @@ drop table if exists `user`;
 
 create table `user` (
     `id` int not null AUTO_INCREMENT primary key,
-    `email` varchar(256) not null check(`email` <> ""),
+    `email` varchar(256) not null unique check(`email` <> ""),
     `password` varchar(64) not null check(`password` <> ""),
     `username` varchar(64) not null check(`username` <> ""),
     `date` datetime not null DEFAULT CURRENT_TIMESTAMP
