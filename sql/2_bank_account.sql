32749b12c46bb38be656930131421c54f30146b0
Zhou Jian <zhoujian0998@gmail.com>
2020-07-29 17:09:38 +0200

    foreign key references bug fix

--- sql/2_bank_account.sql
+++ sql/2_bank_account.sql
@@ -3,8 +3,9 @@ use `sf_academy_exchange_db`;
 drop table if exists `bank_account`;
 
 create table `bank_account` (
-    `user_id` int references `user`.`id` on update cascade on delete cascade,
+    `user_id` int not null,
     `iban` varchar(16) not null check(`iban` <> ""),
     `date` datetime not null DEFAULT CURRENT_TIMESTAMP,
+    foreign key (`user_id`) references `user`(`id`) on update cascade on delete cascade,
     primary key (`user_id`, `iban`)
 )
--- sql/3_virtual_portfolio.sql
+++ sql/3_virtual_portfolio.sql
@@ -4,9 +4,10 @@ drop table if exists `virtual_portfolio`;
 
 create table `virtual_portfolio` (
     `id` int not null AUTO_INCREMENT primary key,
-    `user_id` int references `user`.`id` on update cascade on delete cascade,
+    `user_id` int not null,
     `currency` char(3) not null check(`currency` <> ""),
     `date` datetime not null DEFAULT CURRENT_TIMESTAMP,
-    `balance` decimal(15, 2) not null check(`balance` >= 0) default 0,
+    `balance` decimal(15, 2) not null default 0 check(`balance` >= 0),
+    foreign key (`user_id`) references `user`(`id`) on update cascade on delete cascade,
     constraint user_unique_portfolio unique (`user_id`, `currency`)
 )
