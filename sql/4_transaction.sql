6373bf25cbe3d91b002925f649c542311ff3f031
Zhou Jian <zhoujian0998@gmail.com>
2020-07-31 11:26:45 +0200

    transaction amount > 0

--- sql/4_transaction.sql
+++ sql/4_transaction.sql
@@ -7,7 +7,7 @@ create table `transaction` (
     `virtual_portfolio_src_id` int not null,
     `virtual_portfolio_dest_id` int not null,
     `date` datetime not null DEFAULT CURRENT_TIMESTAMP,
-    `amount` decimal(15, 2) not null check(`amount` >= 0),
+    `amount` decimal(15, 2) not null check(`amount` > 0),
     `rate` decimal(15, 3) not null check (`rate` >= 0),
     foreign key (`virtual_portfolio_src_id`) references `virtual_portfolio`(`id`) on update cascade on delete cascade,
     foreign key (`virtual_portfolio_dest_id`) references `virtual_portfolio`(`id`) on update cascade on delete cascade
